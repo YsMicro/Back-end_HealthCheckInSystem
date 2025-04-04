fabd9c9d10f35329ebc931398df6387a44982bf1
Vojago <ywh2742450799@gmail.com>
2025-04-05 00:58:32 +0800

    refactor(UserController/UserServiceImpl): 使用构造器注入替代字段注入
    将@Autowired字段注入改为构造器注入方式，提升代码可测试性和可维护性

--- src/main/java/edu/vojago/backend_healthcheckinsystem/controller/UserController.java
+++ src/main/java/edu/vojago/backend_healthcheckinsystem/controller/UserController.java
@@ -2,15 +2,17 @@ package edu.vojago.backend_healthcheckinsystem.controller;
 
 import edu.vojago.backend_healthcheckinsystem.pojo.User;
 import edu.vojago.backend_healthcheckinsystem.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @RequestMapping("/findUserById")
     public User findUserById(Integer user_id) {
--- src/main/java/edu/vojago/backend_healthcheckinsystem/service/impl/UserServiceImpl.java
+++ src/main/java/edu/vojago/backend_healthcheckinsystem/service/impl/UserServiceImpl.java
@@ -3,14 +3,16 @@ package edu.vojago.backend_healthcheckinsystem.service.impl;
 import edu.vojago.backend_healthcheckinsystem.mapper.UserMapper;
 import edu.vojago.backend_healthcheckinsystem.pojo.User;
 import edu.vojago.backend_healthcheckinsystem.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Autowired
-    private UserMapper userMapper;
+    private final UserMapper userMapper;
+
+    public UserServiceImpl(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
 
     @Override
     public User findUserById(Integer user_id) {
