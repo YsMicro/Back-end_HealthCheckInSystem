021ba8732edb50c33d3fb80ac8e2272c3629aa72
Vojago <ywh2742450799@gmail.com>
2025-04-04 20:09:07 +0800

    refactor(mapper): 更新 UserMapper 中的 SQL 查询语句
    - 在 SELECT 语句中添加了数据库名 `my healthcheck-in app`，以确保查询的准确性- 使用反引号括起表名，以处理特殊字符

--- src/main/java/edu/vojago/backend_healthcheckinsystem/mapper/UserMapper.java
+++ src/main/java/edu/vojago/backend_healthcheckinsystem/mapper/UserMapper.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
-    @Select("select * from users where user_id = #{user_id}")
+    @Select("select * from `my healthcheck-in app`.users where user_id = #{user_id}")
     User findUserById(Integer user_id);
 
 }
