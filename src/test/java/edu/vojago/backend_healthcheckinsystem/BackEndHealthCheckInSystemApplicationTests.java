000f163c16e00b606e56d0ad1d77145ea37fb4d8
Vojago <ywh2742450799@gmail.com>
2025-04-05 01:05:45 +0800

    fix(pom.xml): 恢复Spring Boot版本注释格式 refactor(test): 移除测试类中未使用的导入和方法

--- src/test/java/edu/vojago/backend_healthcheckinsystem/BackEndHealthCheckInSystemApplicationTests.java
+++ src/test/java/edu/vojago/backend_healthcheckinsystem/BackEndHealthCheckInSystemApplicationTests.java
@@ -1,13 +1,8 @@
 package edu.vojago.backend_healthcheckinsystem;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackEndHealthCheckInSystemApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
