19d733c087d5b0771139552cd95aa84d07ff886e
Vojago <ywh2742450799@gmail.com>
2025-04-04 15:24:13 +0800

    Initial commit

---
+++ src/main/java/edu/vojago/backend_healthcheckinsystem/BackEndHealthCheckInSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.vojago.backend_healthcheckinsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndHealthCheckInSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndHealthCheckInSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/vojago/backend_healthcheckinsystem/BackEndHealthCheckInSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.vojago.backend_healthcheckinsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndHealthCheckInSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
