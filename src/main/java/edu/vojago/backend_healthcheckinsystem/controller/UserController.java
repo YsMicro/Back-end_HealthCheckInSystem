1af342e7c0b93d6738714196eb7ec2676194dac6
Vojago <ywh2742450799@gmail.com>
2025-04-11 13:26:21 +0800

    feat(UserController): 新增用户信息查询接口
    添加/userInfo接口，通过token获取用户详细信息
    
    Signed-off-by: Vojago <ywh2742450799@gmail.com>

--- src/main/java/edu/vojago/backend_healthcheckinsystem/controller/UserController.java
+++ src/main/java/edu/vojago/backend_healthcheckinsystem/controller/UserController.java
@@ -7,9 +7,7 @@ import edu.vojago.backend_healthcheckinsystem.utils.JwtUtil;
 import edu.vojago.backend_healthcheckinsystem.utils.MD5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -71,6 +69,7 @@ public class UserController {
         return Result.error("删除失败");
     }
 
+    //用户登录
     @PostMapping("/login")
     public Result<String> login(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
 
@@ -92,5 +91,15 @@ public class UserController {
         claims.put("username", loginUser.getUsername());
         String token = JwtUtil.generateToken(claims);
         return Result.success(token);
+    }
+
+    //获取用户详细信息
+    @GetMapping("/userInfo")
+    public Result<User> userInfo(@RequestHeader(name = "Authorization") String token) {
+        //通过username查询用户
+        Map<String, Object> map = JwtUtil.verifyToken(token);
+        String username = (String) map.get("username");
+        User user = userService.findUserByName(username);
+        return Result.success(user);
     }
 }
