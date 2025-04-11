ac23cdec43646f027d66d1967e7e0cab3ddaaf9c
Vojago <ywh2742450799@gmail.com>
2025-04-11 22:21:51 +0800

    test: 添加ThreadLocalTest单元测试文件
    Signed-off-by: Vojago <ywh2742450799@gmail.com>

---
+++ src/test/java/edu/vojago/backend_healthcheckinsystem/ThreadLocalTest.java
@@ -0,0 +1,29 @@
+package edu.vojago.backend_healthcheckinsystem;
+
+
+import org.junit.jupiter.api.Test;
+
+public class ThreadLocalTest {
+
+    @Test
+    public void testThreadLocal() {
+        //提供一个ThreadLocal对象
+        ThreadLocal tl = new ThreadLocal();
+
+        //开启两个线程
+        new Thread(() -> {
+            tl.set("张三");
+            System.out.println(Thread.currentThread().getName() + ": " + tl.get());
+            System.out.println(Thread.currentThread().getName() + ": " + tl.get());
+            System.out.println(Thread.currentThread().getName() + ": " + tl.get());
+        }
+                , "蓝色").start();
+        new Thread(() -> {
+            tl.set("李四");
+            System.out.println(Thread.currentThread().getName() + ": " + tl.get());
+            System.out.println(Thread.currentThread().getName() + ": " + tl.get());
+            System.out.println(Thread.currentThread().getName() + ": " + tl.get());
+        }
+                , "绿色").start();
+    }
+}
