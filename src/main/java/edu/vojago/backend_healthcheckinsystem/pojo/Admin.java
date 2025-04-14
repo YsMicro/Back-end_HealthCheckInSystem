7c52ddead69e71f48db2c3db05d1dc1b39212f60
Vojago <ywh2742450799@gmail.com>
2025-04-14 21:23:04 +0800

    refactor(pojo/Admin): 将字段名改为驼峰命名法
    修改Admin类的字段命名风格，从下划线命名改为驼峰命名，包括admin_id→adminId，last_login_ip→lastLoginIp，created_time→createdTime
    
    Signed-off-by: Vojago <ywh2742450799@gmail.com>

--- src/main/java/edu/vojago/backend_healthcheckinsystem/pojo/Admin.java
+++ src/main/java/edu/vojago/backend_healthcheckinsystem/pojo/Admin.java
@@ -6,22 +6,22 @@ import java.time.LocalDateTime;
 
 @Data
 public class Admin {
-    private Integer admin_id;           //管理员唯一ID
+    private Integer adminId;           //管理员唯一ID
     private String username;            //登录账号
     private String password;            //密码（加密）
     private String role;                //角色（super-超级管理员）
-    private String last_login_ip;       //最后登录IP
-    private LocalDateTime created_time; //创建时间
+    private String lastLoginIp;       //最后登录IP
+    private LocalDateTime createdTime; //创建时间
 
     public Admin() {
     }
 
-    public Admin(Integer admin_id, String username, String password, String role, String last_login_ip, LocalDateTime created_time) {
-        this.admin_id = admin_id;
+    public Admin(Integer adminId, String username, String password, String role, String lastLoginIp, LocalDateTime createdTime) {
+        this.adminId = adminId;
         this.username = username;
         this.password = password;
         this.role = role;
-        this.last_login_ip = last_login_ip;
-        this.created_time = created_time;
+        this.lastLoginIp = lastLoginIp;
+        this.createdTime = createdTime;
     }
 }
