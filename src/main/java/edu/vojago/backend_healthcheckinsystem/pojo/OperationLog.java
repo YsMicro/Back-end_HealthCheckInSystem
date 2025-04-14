bfcfba9ef3d2b2b81c18b2ab8e291e3e37647d24
Vojago <ywh2742450799@gmail.com>
2025-04-14 20:53:23 +0800

    refactor(pojo/OperationLog): 将字段名改为驼峰命名法
    Signed-off-by: Vojago <ywh2742450799@gmail.com>

--- src/main/java/edu/vojago/backend_healthcheckinsystem/pojo/OperationLog.java
+++ src/main/java/edu/vojago/backend_healthcheckinsystem/pojo/OperationLog.java
@@ -6,22 +6,22 @@ import java.time.LocalDateTime;
 
 @Data
 public class OperationLog {
-    private Integer log_id;             //日志唯一ID
-    private Integer admin_id;           //操作管理员ID
-    private String action_type;         //操作类型（如"禁用用户"）
-    private Integer target_id;          //操作目标ID（如"用户ID"）
+    private Integer logId;             //日志唯一ID
+    private Integer adminId;           //操作管理员ID
+    private String actionType;         //操作类型（如"禁用用户"）
+    private Integer targetId;          //操作目标ID（如"用户ID"）
     private String detail;              //操作详情（JSON格式）
-    private LocalDateTime create_time;  //操作时间
+    private LocalDateTime createTime;  //操作时间
 
     public OperationLog() {
     }
 
-    public OperationLog(Integer log_id, Integer admin_id, String action_type, Integer target_id, String detail, LocalDateTime create_time) {
-        this.log_id = log_id;
-        this.admin_id = admin_id;
-        this.action_type = action_type;
-        this.target_id = target_id;
+    public OperationLog(Integer logId, Integer adminId, String actionType, Integer targetId, String detail, LocalDateTime createTime) {
+        this.logId = logId;
+        this.adminId = adminId;
+        this.actionType = actionType;
+        this.targetId = targetId;
         this.detail = detail;
-        this.create_time = create_time;
+        this.createTime = createTime;
     }
 }
