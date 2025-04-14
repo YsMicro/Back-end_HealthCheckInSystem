dba270187f737ca998ef306e30c356527922652a
Vojago <ywh2742450799@gmail.com>
2025-04-14 20:54:01 +0800

    refactor(ThreadLocalUtil): 增加泛型支持并添加管理员ID获取方法
    修改ThreadLocalUtil类，使用泛型ThreadLocal<Map>替代原始类型，新增getAdminId方法用于获取管理员ID
    
    Signed-off-by: Vojago <ywh2742450799@gmail.com>

--- src/main/java/edu/vojago/backend_healthcheckinsystem/utils/ThreadLocalUtil.java
+++ src/main/java/edu/vojago/backend_healthcheckinsystem/utils/ThreadLocalUtil.java
@@ -1,10 +1,11 @@
 package edu.vojago.backend_healthcheckinsystem.utils;
 
+import java.util.Map;
 
 public class ThreadLocalUtil {
 
     //提供ThreadLocal对象
-    private static final ThreadLocal THREAD_LOCAL = new ThreadLocal();
+    private static final ThreadLocal<Map<String, Object>> THREAD_LOCAL = new ThreadLocal<>();
 
     //根据键获取值
     public static <T> T get() {
@@ -12,13 +13,18 @@ public class ThreadLocalUtil {
     }
 
     //存储键值对
-    public static void set(Object value) {
+    public static void set(Map<String, Object> value) {
         THREAD_LOCAL.set(value);
     }
 
-    //清楚ThreadLocal，防止内存泄露
+    //清除ThreadLocal，防止内存泄露
     public static void remove() {
         THREAD_LOCAL.remove();
     }
 
+    //获取管理员ID
+    public static Integer getAdminId() {
+        Map<String, Object> claims = THREAD_LOCAL.get();
+        return (Integer) claims.get("admin_id");
+    }
 }
